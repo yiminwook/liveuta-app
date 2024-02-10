3a45d5e5bbe6dcfe333a0e0402d6bb21681c4184
yiminwook <grs0416@gmail.com>
2024-02-10 22:33:53 +0900

    Update: config, gitignore

--- app.config.ts
+++ app.config.ts
@@ -1,12 +1,12 @@
-import "ts-node/register"; // Add this to import TypeScript files
-import { ExpoConfig } from "expo/config";
+import 'ts-node/register'; // Add this to import TypeScript files
+import { ExpoConfig } from 'expo/config';
 
 // In SDK 46 and lower, use the following import instead:
 // import { ExpoConfig } from '@expo/config-types';
 
 const config: ExpoConfig = {
-  name: "my-app",
-  slug: "my-app",
+  name: 'LiveUta',
+  slug: 'liveuta-app',
 };
 
 export default config;
